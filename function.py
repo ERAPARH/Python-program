9e5d0f3aaa7c70b2d23ad286be4fb4dcc00a6c0a
ERAPARH <panwar.hari69@gmail.com>
2025-09-27 23:55:44 +0530

    V1

---
+++ function.py
@@ -0,0 +1,23 @@
+def student(name,age,rollnumber):
+    print(f"student name is {name}, and age is {age}, and is rollnumber is{rollnumber}")
+
+student(name="Hariom",age= 32, rollnumber="EGIL74644")
+student(name="Anoop", age=34, rollnumber="EGIL74657")
+
+def greet():
+    print("Thankyou")
+
+greet()
+
+def sum(a,b,c):
+    result = a+b+c
+    return result
+result =sum(21,12,13)
+print(result)
+
+def Max(a,b,c):
+    result = max(a,b,c)
+
+    return result
+result =max(21,133,54)
+print(result)
